0ef033ddee3967e92ec6594028d6ebf2523c3b5d
Guillaume TILLARD <guillaumetillard10@gmail.com>
2024-12-18 10:34:42 +0100

    add & edit formProduct done

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Products;
 use App\Form\ProductsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/products', name: 'admin_products_')]
 class ProductsController extends AbstractController
@@ -17,7 +20,8 @@ class ProductsController extends AbstractController
         return $this->render('admin/products/index.html.twig');
     }
     #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    // j'appelle request car je dois récupérer la requete du form, em pour envoyer en bdd et slugger pour créer un slug
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
@@ -27,16 +31,68 @@ class ProductsController extends AbstractController
         // on crée le formulaire:
         $productForm = $this->createForm(ProductsFormType::class, $product);
 
+        // on traite la requête du form:
+        $productForm->handleRequest($request);
+
+        // on vérifie si le form est soumis et valid
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // on génère le slug:
+            $slug = $slugger->slug($productForm->getName());
+            // on le stock dans produit:
+            $product->setSlug($slug);
+            // on arrondit le prix pour la bdd:
+            $prix = $product->getPrice() * 100;
+            $product->setPrice($prix);
+
+            // on stocke en bdd:
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit ajouté avec succès');
+            return $this->redirectToRoute('admin_products_index');
+        }
+
         return $this->render('admin/products/add.html.twig', [
             'productForm' => $productForm->createView()
         ]);
     }
     #[Route('/edition/{id}', name: 'edit')]
-    public function edit(Products $product): Response
+    public function edit(Products $product, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         // on vérifie si l'utilisateur peut éditer avec le voter, on y envoie l'attribut et un obket product comme dans la function supports dans ProductsVoter
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
-        return $this->render('admin/products/index.html.twig');
+        // Avant le formulaire on divise le prix par 100:
+        // on arrondit le prix pour la bdd:
+        $prix = $product->getPrice() / 100;
+        $product->setPrice($prix);
+
+        // on crée le formulaire:
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        // on traite la requête du form:
+        $productForm->handleRequest($request);
+
+        // on vérifie si le form est soumis et valid
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // on génère le slug:
+            $slug = $slugger->slug($productForm->getName());
+            // on le stock dans produit:
+            $product->setSlug($slug);
+            // on arrondit le prix pour la bdd:
+            $prix = $product->getPrice() * 100;
+            $product->setPrice($prix);
+
+            // on stocke en bdd:
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit modifié avec succès');
+            return $this->redirectToRoute('admin_products_index');
+        }
+
+        return $this->render('admin/products/edit.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
     #[Route('/suppression/{id}', name: 'delete')]
     public function delete(Products $product): Response
